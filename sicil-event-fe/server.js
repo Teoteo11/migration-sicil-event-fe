273e44beb3d6cd99939c26e6f526b6c2420b6a5c
Teoteo11 <depaym69@gmail.com>
2022-05-14 20:30:42 +0200

    first try 3

--- sicil-event-fe/server.js
+++ sicil-event-fe/server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/sicil-event-fe'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/sicil-event-fe/'}),
 );
 
 // Start the app by listening on the default Heroku port
